cdee3b83ec7128ff71ed8d98797020af46c8269c
Ian Douglas Scott <ian@iandouglasscott.com>
2017-10-19 20:41:42 -0700

    WIP: Initial work on Rust 'cook' with ion recipes

---
+++ src/bin/cook.rs
@@ -0,0 +1,104 @@
+extern crate ion_shell;
+extern crate pkgutils;
+
+use std::path::Path;
+use std::fs::{self, File};
+use std::io::Write;
+use std::env;
+use pkgutils::{PackageMeta, Repo};
+use ion_shell::{Builtin, Shell};
+use ion_shell::shell::library::IonLibrary;
+use ion_shell::shell::flags::ERR_EXIT;
+
+fn main() {
+    let target = env!("PKG_DEFAULT_TARGET");
+
+    let builtins = Builtin::map();
+    let mut shell = Shell::new(&builtins);
+    shell.flags |= ERR_EXIT;
+
+    let recipe = Path::new("recipe.ion");
+    if !recipe.exists() {
+        eprintln!("No recipe.ion in current directory");
+    }
+    shell.execute_script(recipe).unwrap();
+
+    let cmd = env::args().nth(1).unwrap();
+
+    match cmd.as_str() {
+        "dist" => {
+            //prepare
+            //build
+            stage(&mut shell);
+            tar(&shell, target);
+        }
+        "distclean" => { /*XXX*/ }
+        "fetch" => { /*XXX*/ }
+        "unfetch" => { /*XXX*/ }
+        "status" => { /*XXX*/ }
+        "status_origin" => { /*XXX*/ }
+        "status_upstream" => { /*XXX*/ }
+        "diff" => { /*XXX*/ }
+        "diff_origin" => { /*XXX*/ }
+        "diff_upstream" => { /*XXX*/ }
+        "difftool" => { /*XXX*/ }
+        "difftool_origin" => { /*XXX*/ }
+        "difftool_upstream" => { /*XXX*/ }
+        "update" => { /*XXX*/ }
+        "prepare" => { /*XXX*/ }
+        "unprepare" => { /*XXX*/ }
+        "version" => { /*XXX*/ }
+        "gitversion" => { /*XXX*/ }
+        "build" => { /*XXX*/ }
+        "test" => { /*XXX*/ }
+        "clean" => { /*XXX*/ }
+        "stage" => stage(&mut shell),
+        "unstage" => { /*XXX*/ }
+        "tar" => tar(&shell, target),
+        "untar" => { /*XXX*/ }
+        "publish" => { /*XXX*/ }
+        "unpublish" => { /*XXX*/ }
+        _ => { /*XXX*/ }
+    }
+}
+
+fn tar(shell: &Shell, target: &str) {
+    let version = "1"; // XXX
+    let name = shell.variables.get_var("NAME").expect("Package missing NAME");
+    let meta = PackageMeta {
+        name: name.clone(),
+        version: version.to_string(),
+        target: target.to_string(),
+    };
+
+    fs::create_dir_all("stage/pkg").unwrap();
+    let mut manifest = File::create(format!("stage/pkg/{}.toml", name)).unwrap();
+    manifest.write_all(meta.to_toml().as_bytes()).unwrap();
+    drop(manifest);
+
+    let repo = Repo::new(target);
+    repo.create("stage").unwrap();
+}
+
+fn call_func(shell: &mut Shell, func: &str, args: &[&str]) -> bool {
+    shell.variables.set_var("skip", "0");
+    if shell.functions.contains_key(func) {
+        let mut cmd = func.to_string();
+        // NOTE no escaping is performed
+        for arg in args {
+            cmd.push(' ');
+            cmd.push_str(arg);
+        }
+        shell.execute_command(&cmd);
+    }
+    shell.variables.get_var("skip") == Some("1".to_string())
+}
+
+fn unstage() {
+}
+
+fn stage(shell: &mut Shell) {
+    unstage();
+    fs::create_dir("stage");
+    let skip = call_func(shell, "stage", &["./stage"]);
+}
