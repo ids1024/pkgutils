417262676d937aa3dd97aa7e5d29a94f6a9e4dad
Ian Douglas Scott <ian@iandouglasscott.com>
2017-10-28 14:46:16 -0700

    Use clap in cook

--- src/bin/cook.rs
+++ src/bin/cook.rs
@@ -1,14 +1,25 @@
 extern crate ion_shell;
 extern crate pkgutils;
+extern crate clap;
 
 use std::path::Path;
-use std::env;
 
 use pkgutils::Recipe;
+use clap::{App, Arg};
 
 fn main() {
-    let target = env!("PKG_DEFAULT_TARGET");
+    let matches = App::new("cook")
+        .arg(Arg::with_name("target")
+             .long("target")
+             .takes_value(true)
+             )
+        .arg(Arg::with_name("command")
+             .multiple(true)
+             .required(true)
+             )
+        .get_matches();
 
+    let target = matches.value_of("target").unwrap_or(env!("PKG_DEFAULT_TARGET"));
 
     let recipe_path = Path::new("recipe.ion");
     if !recipe_path.exists() {
@@ -17,41 +28,41 @@ fn main() {
 
     let mut recipe = Recipe::new(recipe_path);
 
-    let cmd = env::args().nth(1).unwrap();
-
-    match cmd.as_str() {
-        "dist" => {
-            //prepare
-            //build
-            recipe.stage();
-            recipe.tar(target);
+    for cmd in matches.values_of("command").unwrap() {
+        match cmd {
+            "dist" => {
+                //prepare
+                //build
+                recipe.stage();
+                recipe.tar(target);
+            }
+            "distclean" => { /*XXX*/ }
+            "fetch" => recipe.fetch(),
+            "unfetch" => recipe.unfetch(),
+            "status" => { /*XXX*/ }
+            "status_origin" => { /*XXX*/ }
+            "status_upstream" => { /*XXX*/ }
+            "diff" => { /*XXX*/ }
+            "diff_origin" => { /*XXX*/ }
+            "diff_upstream" => { /*XXX*/ }
+            "difftool" => { /*XXX*/ }
+            "difftool_origin" => { /*XXX*/ }
+            "difftool_upstream" => { /*XXX*/ }
+            "update" => { /*XXX*/ }
+            "prepare" => { /*XXX*/ }
+            "unprepare" => recipe.unprepare(),
+            "version" => { /*XXX*/ }
+            "gitversion" => { /*XXX*/ }
+            "build" => { /*XXX*/ }
+            "test" => { /*XXX*/ }
+            "clean" => { /*XXX*/ }
+            "stage" => recipe.stage(),
+            "unstage" => recipe.unstage(),
+            "tar" => recipe.tar(target),
+            "untar" => { /*XXX*/ }
+            "publish" => { /*XXX*/ }
+            "unpublish" => { /*XXX*/ }
+            _ => { /*XXX*/ }
         }
-        "distclean" => { /*XXX*/ }
-        "fetch" => recipe.fetch(),
-        "unfetch" => recipe.unfetch(),
-        "status" => { /*XXX*/ }
-        "status_origin" => { /*XXX*/ }
-        "status_upstream" => { /*XXX*/ }
-        "diff" => { /*XXX*/ }
-        "diff_origin" => { /*XXX*/ }
-        "diff_upstream" => { /*XXX*/ }
-        "difftool" => { /*XXX*/ }
-        "difftool_origin" => { /*XXX*/ }
-        "difftool_upstream" => { /*XXX*/ }
-        "update" => { /*XXX*/ }
-        "prepare" => { /*XXX*/ }
-        "unprepare" => recipe.unprepare(),
-        "version" => { /*XXX*/ }
-        "gitversion" => { /*XXX*/ }
-        "build" => { /*XXX*/ }
-        "test" => { /*XXX*/ }
-        "clean" => { /*XXX*/ }
-        "stage" => recipe.stage(),
-        "unstage" => recipe.unstage(),
-        "tar" => recipe.tar(target),
-        "untar" => { /*XXX*/ }
-        "publish" => { /*XXX*/ }
-        "unpublish" => { /*XXX*/ }
-        _ => { /*XXX*/ }
     }
 }
